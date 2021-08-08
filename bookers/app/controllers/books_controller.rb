752382075a8f5818d6260600ed31b2bd89a16b8b
ybnr1130 <muuckk@gmail.com>
2021-08-08 14:17:29 +0000

    [Update] Showビューへの接続機能追加

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,20 +1,43 @@
 class BooksController < ApplicationController
+# エラー文が表示されない、CreateアクションのちにShow画面へ遷移しない、URLがbooks/:idではなくbooks.:idになる
+# 関係しない箇所はコメントアウトにしてあるよ
+
+
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    # @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_id_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
+  def update
+    @book = Book.find(params[:id])
+    book.update
+    redirect_to books_id_path
+  end
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_index_path
+    redirect_to books_index
   end
   
   
--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
-  get 'books/index' => 'books#index'
-  get 'books/:id' => 'books#show'
-  get 'books/new' => 'books#index'
-  get 'books/:id/edit' => 'books#edit'
+  get 'books' => 'books#index', as: 'books_index'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'books_id' 
+  get 'books/new' => 'books#new', as: 'books_new'
+  get 'books/:id/edit' => 'books#edit', as: 'books_edit'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
